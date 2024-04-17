a173ed6b799ef1fd9cb1f7689c255595f05491d1
jwlim-ssts <jwlim@ssts.co.kr>
2024-04-17 12:44:52 +0900

    ch02 - 2.4

--- src/main/java/ch02/Ch2Test.java
+++ src/main/java/ch02/Ch2Test.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+
 public class Ch2Test {
 
     @Test
@@ -19,6 +21,7 @@ public class Ch2Test {
     public void cashConstructor(){
         Cash x = new Cash(29,95,"USD");
         Cash y = new Cash(29,95,"USD");
+        Assertions.assertEquals(x.hashCode(),y.hashCode());
         Assertions.assertEquals(x,y);
     }
 
@@ -30,5 +33,20 @@ public class Ch2Test {
 
         Employee employeeUsd = new Employee(new UsdCash());
         Assertions.assertEquals(Integer.MIN_VALUE,employeeUsd.toCents());
+    }
+
+    @Test
+    @DisplayName("withoutHashCode")
+    public void withoutHashCode(){
+        HashSet<Cash> set = new HashSet<>();
+
+        Cash x = new Cash(29,95,"USD");
+        Cash y = new Cash(29,95,"USD");
+
+        set.add(x);
+        set.add(y);
+
+        Assertions.assertEquals(1,set.size());
+        Assertions.assertNotEquals(2,set.size());
     }
 }
